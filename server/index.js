167cf94ede9b953274b982b50ed61bdee520b207
DevokSoftware <devok.software@gmail.com>
2022-10-21 21:47:34 +0200

    added some logs to backend server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const NBA = require("nba");
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
@@ -8,9 +7,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT)
 });
 
 app.get("/stats", async (req, res) => {
+    console.log('Requesting stats...')
     let stats =
         await NBA.stats.teamStats({ Season: '2022-23' });
     res.send(stats)
