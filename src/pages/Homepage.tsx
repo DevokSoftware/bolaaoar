bc131337eeab8a0f456711d183898fee76dd434b
Leonel Goncalves <devok.software@gmail.com>
2024-10-24 15:25:15 +0200

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -61,7 +61,7 @@ export const Homepage: React.FC = () => {
 
   React.useEffect(() => {
     // fetch("http://localhost:3001/standings")
-    fetch("https://bolaaoar.onrender.com/standings")
+    fetch("https://elated-tuxedo-mite.cyclic.app/standings")
       .then((response) => response.json())
       .then((data) => mapTeams(data));
   }, []);
